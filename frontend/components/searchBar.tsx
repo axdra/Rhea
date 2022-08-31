f562620aa4e775459a6d649fd12d9ec42fa3fdf9
axdra <axel.draws@outlook.com>
2022-08-31 18:08:08 +0200

    fixed error

--- frontend/components/searchBar.tsx
+++ frontend/components/searchBar.tsx
@@ -1,17 +1,18 @@
 import { useRouter } from "next/router";
-import { FC, FormEvent, FormEventHandler } from "react";
+import { FC, FormEvent, FormEventHandler, useState } from "react";
 
 const SearchBar: FC = () => {
     const router = useRouter()
-    
+    const [searchQuary, setSearchQuary] = useState("")
     const search = (e: FormEvent) => {
 
         e.preventDefault();
         //redirect to next search page
-        router.push(`/search?q=${e.currentTarget.searchquary.value}`)
+        
+        router.push(`/search?q=${searchQuary}`)
     }
     return <form onSubmit={search}>
-    <input type="text" name="searchquary" className="rounded-full border-gray-300 placeholder:text-gray-300 focus:ring-orange-500 focus:border-orange-500 shadow-sm" placeholder="Search" />
+    <input type="text" value={searchQuary} onChange={(e)=>setSearchQuary(e.currentTarget.value)} name="searchquary" className="rounded-full border-gray-300 placeholder:text-gray-300 focus:ring-orange-500 focus:border-orange-500 shadow-sm" placeholder="Search" />
     </form>
 }
 export default SearchBar;
