3cd072f87a28da2e06d98426621e511af98ef882
axdra <axel.draws@outlook.com>
2022-09-03 13:03:06 +0200

    404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from "next";
+import Link from "next/link";
 
 const fourOFour: NextPage = () => { 
 
     return (
-        <div className="h-full flex justify-center items-center">
-        <h1>whoops 404</h1>
+        <div className="h-full flex flex-col justify-center items-center flex-1">
+            <h1>whoops 404</h1>
+            <Link href="/">
+                <a className="hover:underline text-orange-500">Go back home</a>
+            </Link>
         </div>
     );
 }
