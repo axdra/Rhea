602e432de81bcc6d533302be6a7b871457a6ba56
axdra <axel.draws@outlook.com>
2022-09-05 12:53:48 +0200

    Sort out already past events

--- frontend/pages/api/events.ts
+++ frontend/pages/api/events.ts
@@ -14,22 +14,24 @@ export default function handler(
     }
       if (!Array.isArray(calendarCode) && calendarCode){
           supabase.from('Calendars').select("*, Events(*)").limit(1).ilike('code', calendarCode).single().then(data => {
-             const  events = data.data.Events.map((event: any) => {
-                  return {
+            const events = data.data.Events.filter((ev:any)=>new Date(ev.end_date) > new Date()).map((event: any) => {
+              
+                return {
                       
-                        aid: event.aid,
-                        created_at: new Date(event.created_at.replace('T', 'Z')),
-                        end_date: new Date(event.end_date.replace('T', 'Z')),
-                        group:  event.group,
-                        id: event.id,
-                        last_update: new Date(event.last_update.replace('T', 'Z')),
-                        name:   event.name,
-                        parent_calendar:    event.parent_calendar,
-                        room: event.room,
-                        start_date: new Date(event.start_date.replace('T', 'Z')),
-                        teacher: event.teacher,
-                  }
-              }
+                  aid: event.aid,
+                  created_at: new Date(event.created_at.replace('T', 'Z')),
+                  end_date: new Date(event.end_date.replace('T', 'Z')),
+                  group: event.group,
+                  id: event.id,
+                  last_update: new Date(event.last_update.replace('T', 'Z')),
+                  name: event.name,
+                  parent_calendar: event.parent_calendar,
+                  room: event.room,
+                  start_date: new Date(event.start_date.replace('T', 'Z')),
+                  teacher: event.teacher,
+                }
+              
+            }
                 )
               res.status(200).json({course: data.data.course, name: data.data.name, code: data.data.code, Events: events })
         }
