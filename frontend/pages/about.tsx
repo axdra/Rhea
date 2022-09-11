22b5cf9ef59564b0616a6b7b7d1aff0603dc4443
Axel Draws <axel.draws@outlook.com>
2022-09-11 02:28:02 +0200

    added padding

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const fourOFour: NextPage = () => {
 
     return (
-        <div className="h-full flex flex-col justify-center items-center flex-1">
+        <div className="h-full flex flex-col justify-center items-center flex-1 px-4">
             <p>Created by <a className="text-orange-500 hover:underline hover:text-orange-600" href="https://axeldraws.com">Axel Draws</a></p>
             <p>With help from <a className="text-orange-500 hover:underline hover:text-orange-600" href="https://linkedin.com/in/oskar-sturebrand-aa73b7219">Oskar Sturebrand</a> creating maps.</p>
         </div>
