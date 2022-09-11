95e03d82a1c83cf87be5bc2ec12ba157d0e214ab
axdra <axel.draws@outlook.com>
2022-09-11 18:27:28 +0200

    Courses

--- backend/calendar/courses_scraper.py
+++ backend/calendar/courses_scraper.py
@@ -2,6 +2,7 @@
 #handle args
 import argparse
 import os
+from time import sleep
 
 
 if __name__ == '__main__':
@@ -14,6 +15,9 @@ if __name__ == '__main__':
     parser.add_argument('--supabase-url', help='Supabase project URL, defaults to env variable', default=supabase_url )
     parser.add_argument('--supabase-anon-key', help='Supabase anon key, defaults to env variable', default=supabase_anon_key )
     parser.add_argument('--supabase-secret-key', help='Supabase secret key, defaults to env variable', default=supabase_secret_key )
+    parser.add_argument('--webdriver-path', help='Path to the webdriver for scraper' )
+    parser.add_argument('--webdriver', help='Type of webdriver', default='edge' )
+    parser.add_argument('--course-url', help='URL to the page of courses', default="https://webbschema.mdu.se/allaKurser.jsp" )
     args = parser.parse_args()
 
     # check if variables are set
@@ -23,7 +27,37 @@ if __name__ == '__main__':
         raise TypeError('No supabase anon key set')
     if not supabase_secret_key:
         raise TypeError('No supabase secret key set')
+
+    #get webdriver
+    if args.webdriver == 'edge':
+        driver = webdriver.Edge(args.webdriver_path)
+    elif args.webdriver == 'chrome':
+        driver = webdriver.Chrome(args.webdriver_path)
+    elif args.webdriver == 'firefox':
+        driver = webdriver.Firefox(args.webdriver_path)
+    else:
+        raise TypeError('Invalid webdriver')
     
+    courses_list = []
+
+    driver.get(args.course_url)
+    sleep(2)
+    elems = driver.find_element(By.CSS_SELECTOR, "#resultatdiv")
+    soup = BeautifulSoup(elems.get_attribute('innerHTML'), 'html.parser')
+
+    for course in soup.find_all('li'):
+        course_URL = "https://webbschema.mdu.se/"+course.find('a', href=True)['href']
+        course_name = course.find('a', href=True).text.strip()
+        course_code = course.text.split('(')[1].split(')')[0] 
+        course_obj = {
+            "name": course_name,
+            "URL": course_URL,
+            "code":  course_code 
+        }
+        courses_list.append(course_obj)
+    supabase.table('Courses').insert(courses_list).execute()
+    driver.close()
+    driver.quit()
         
 
 
--- backend/main.py
+++ backend/main.py
@@ -30,26 +30,7 @@ def json_serial(obj):
 
 def get_courses():
 
-    courses_list = []
-    driver = webdriver.Edge(executable_path='C:\\Users\\Axel\\Desktop\\edgedriver_win64\\msedgedriver.exe')
-    driver.get(test_url)
-    sleep(2)
-    elems = driver.find_element(By.CSS_SELECTOR, "#resultatdiv")
-    soup = BeautifulSoup(elems.get_attribute('innerHTML'), 'html.parser')
-
-    for course in soup.find_all('li'):
-        course_URL = "https://webbschema.mdu.se/"+course.find('a', href=True)['href']
-        course_name = course.find('a', href=True).text.strip()
-        course_code = course.text.split('(')[1].split(')')[0] 
-        course_obj = {
-            "name": course_name,
-            "URL": course_URL,
-            "code":  course_code 
-        }
-        courses_list.append(course_obj)
-    supabase.table('Courses').insert(courses_list).execute()
-    driver.close()
-    driver.quit()
+  
 
 
 def get_calendars():
