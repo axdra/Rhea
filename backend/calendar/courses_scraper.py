944769b446de6bbd4cbc1e3212c7be1cfa61b2da
axdra <axel.draws@outlook.com>
2022-09-24 09:28:36 +0200

    Changes to web scraper

--- backend/calendar/courses_scraper.py
+++ backend/calendar/courses_scraper.py
@@ -4,6 +4,7 @@ import argparse
 import os
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from supabase import create_client, Client
 
@@ -11,12 +12,10 @@ from supabase import create_client, Client
 if __name__ == '__main__':
     # get env variables
     supabase_url = os.getenv('SUPABASE_URL')
-    supabase_anon_key = os.getenv('SUPABASE_ANON_KEY')
     supabase_secret_key = os.getenv('SUPABASE_SECRET_KEY')
     #parse args
     parser = argparse.ArgumentParser()
     parser.add_argument('--supabase-url', help='Supabase project URL, defaults to env variable', default=supabase_url )
-    parser.add_argument('--supabase-anon-key', help='Supabase anon key, defaults to env variable', default=supabase_anon_key )
     parser.add_argument('--supabase-secret-key', help='Supabase secret key, defaults to env variable', default=supabase_secret_key )
     parser.add_argument('--webdriver-path', help='Path to the webdriver for scraper' )
     parser.add_argument('--webdriver', help='Type of webdriver', default='edge' )
@@ -24,11 +23,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # check if variables are set
-    if not supabase_url or not args.supabase_url:
+    if not supabase_url and not args.supabase_url:
         raise TypeError( 'No supabase url set')
-    if not supabase_anon_key or not args.supabase_anon_key:
-        raise TypeError('No supabase anon key set')
-    if not supabase_secret_key or not args.supabase_secret_key:
+    if not supabase_secret_key and not args.supabase_secret_key:
         raise TypeError('No supabase secret key set')
 
     #get webdriver
@@ -54,13 +51,13 @@ if __name__ == '__main__':
         course_code = course.text.split('(')[1].split(')')[0] 
         course_obj = {
             "name": course_name,
-            "URL": course_URL,
+            "url": course_URL,
             "code":  course_code 
         }
         courses_list.append(course_obj)
     sb_client:Client = create_client(args.supabase_url, args.supabase_secret_key)
-    sb_client.table('Courses').delete().neq('id',0).execute()
-    sb_client.table('Courses').insert(courses_list).execute()
+    sb_client.table('courses').delete().neq('id',0).execute()
+    sb_client.table('courses').insert(courses_list).execute()
     driver.close()
     driver.quit()
         
