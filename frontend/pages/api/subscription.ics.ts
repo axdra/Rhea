34a13073d6f9c1ab92696e9242480b24b88e9855
axdra <axel.draws@outlook.com>
2022-09-26 13:18:26 +0200

    Fixed major bug with subscription

--- frontend/pages/api/subscription.ics.ts
+++ frontend/pages/api/subscription.ics.ts
@@ -20,8 +20,8 @@ export default async function handler(
            
             const ics = require('ics');
             const calEvents = events?.events.map((event:any) => {
-                const start = moment(event.start_date).format('YYYY-M-D-H-m').split("-").map((item, index) => { return parseInt(item) });
-                const end = moment(event.end_date).format('YYYY-M-D-H-m').split("-").map((item, index) => { return parseInt(item) });
+                const start = moment(event.start_time).format('YYYY-M-D-H-m').split("-").map((item, index) => { return parseInt(item) });
+                const end = moment(event.end_time).format('YYYY-M-D-H-m').split("-").map((item, index) => { return parseInt(item) });
                 let url = "";
                 if (Rooms.data?.some((room: any) => (room.name as string).toLowerCase() === (event.room.split(' ')[0] as string).toLowerCase())) {
                     url = 'https://'+mapURL+'/map?q=' + event.room.split(' ')[0]
