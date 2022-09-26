5dcd3889b8f860fd005dd5b2b7ba5e4a3cee00e6
axdra <axel.draws@outlook.com>
2022-09-26 13:50:23 +0200

    Small darkmode issue

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
         <div className='dark:bg-black dark:border dark:border-white dark:text-white bg-white md:shadow-md rounded px-8 pt-6 pb-8 mb-4 relative '>
           <div className='h-4 w-4 bg-orange-500 animate-ping rounded-full left-4  md:left-auto md:-right-2 -top-2 absolute '></div>
           <div className='h-4 w-4  bg-orange-500 rounded-full left-4 md:left-auto  md:-right-2 -top-2 absolute group'>
-            <div className='px-4 py-4 rounded-lg bg-white hidden  shadow absolute group-hover:block w-64 md:-left-32 top-3'>
+            <div className='px-4 py-4 rounded-lg dark:bg-black dark:border dark:border-white bg-white hidden  shadow absolute group-hover:block w-64 md:-left-32 top-3'>
               <h1 className='font-bold'>
                 {t('stilInDevelopment')}
               </h1>
