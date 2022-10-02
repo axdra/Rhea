48674a515d15f7769cb34d343c0a6a2e09bccdb2
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2022-10-02 16:25:05 +0200

    Remove policy create checks

--- supabase/migrations/20221002133828_union_storage_rls.sql
+++ supabase/migrations/20221002133828_union_storage_rls.sql
@@ -1,14 +1,14 @@
 insert into storage.buckets (id, name)
 values ('unions', 'unions') on conflict (id) do nothing;
 
-create policy if not exists "Union images are publicly accessible."
+create policy "Union images are publicly accessible."
   on storage.objects for select
   using ( bucket_id = 'unions' );
 
-create policy if not exists "Anyone can upload a unions image."
+create policy "Anyone can upload a unions image."
   on storage.objects for insert
   with check ( bucket_id = 'unions' );
 
-create policy if not exists "Only creator can update"
+create policy "Only creator can update"
 on storage.objects for update
 using ( auth.uid() = owner );
