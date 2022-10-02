a6148c686c898fe1a071a268b25bbfa69dc15d61
axeldraws <axel.draws@outlook.com>
2022-10-02 02:32:53 +0200

    shorten commit for no real reason

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -47,8 +47,8 @@ const fourOFour: NextPage = () => {
                             <a href="https://github.com/axdra/Rhea">
                                 <FaGithubSquare className="h-12 w-12 text-black dark:text-white mx-auto" />
                             </a>
-                            <label className="text-neutral-300 dark:text-neutral-700">
-                                Commit: <a className="text-neutral-400 dark:text-neutral-600" href={`https://github.com/axdra/Rhea/commit/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || ''}`} >{process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || "master"}</a>
+                            <label className="text-neutral-300 dark:text-neutral-700 mb-10">
+                                Commit: <a className="text-neutral-400 dark:text-neutral-600" href={`https://github.com/axdra/Rhea/commit/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || ''}`} >{process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0,10) || "master"}</a>
                             </label>
                     </section>
                     </div>
