a8468d6ce111664f3cacc60f4fb32d919537b464
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2022-10-03 21:21:29 +0200

    Improve search?

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,80 +1,78 @@
-import { NextPage } from "next";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSidePropsContext, NextPage } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Database } from "../../types.gen";
 
-const Search: NextPage = () => {
-    //get search query from url
-    const router = useRouter();
-    const searchQuary = router.query.q as string;
-    const [courses, setCourses] = useState<any[]>([]);
-    const { t } = useTranslation();
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        if (searchQuary) {
-            fetch('/api/search?q=' + searchQuary).then
-                (res => res.json()).then(data => {
+type Course = Database["public"]["Tables"]["courses"]["Row"];
 
-                    setCourses(data);
-                }
-                ).finally(() => {
-                    setLoading(false);
-                }
-                )
-        }
-    }
-        , [searchQuary]);
-    return (
-        <div className="max-w-6xl mx-auto md:mt-12 mt-4 md:px-24 px-4 py-10 shadow rounded-lg mb-24 flex-1 flex flex-col xl:dark:border dark:border-white dark:text-white     ">
-            <h1 className="text-4xl font-medium mb-5 xl:w-[72rem] transition-all  ">{t('searchResultsFor', { name: searchQuary })}</h1>
+type Props = {
+  courses?: Course[];
+};
 
-            {courses.length === 0 && !loading && <div className="flex-1 min-h-full flex justify-center "><h2>{t('noResults')}</h2></div>}
-            {loading && <div className="flex-1 min-h-full flex justify-center ">
-                <svg className="animate-spin  h-6 w-6 text-orange-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-            </div>}
-            <div >
+const Search: NextPage<Props> = ({ courses }) => {
+  const { t } = useTranslation();
+  const router = useRouter();
 
-                <ul className="flex flex-col gap-5">
-                    {courses.length > 0 && courses.map(course =>
-                        <Link
-                            key={course.code}
-                            href={{
-                            pathname: "/course",
-                            query: {
-                                q: course.code
-                            }
-                        }}
-                            className="flex sm:flex-row gap-6 flex-col items-center justify-between border rounded-xl dark:bg-black dark:border-white dark:hover:bg-orange-700 border-gray-200 bg-orange-50/20 py-5 px-4 shadow-sm sm:gap-2 hover:bg-gray-50 hover:shadow transition-all duration-300">
+  return (
+    <div className="max-w-6xl mx-auto md:mt-12 mt-4 md:px-24 px-4 py-10 shadow rounded-lg mb-24 flex-1 flex flex-col xl:dark:border dark:border-white dark:text-white     ">
+      <h1 className="text-4xl font-medium mb-5 xl:w-[72rem] transition-all  ">
+        {t("searchResultsFor", { name: router.query.q })}
+      </h1>
 
-                            <div>
-                                <h2>
-                                    {course.name}
-                                </h2>
-                                <h3 className="font-medium">
-                                    {course.code}
-                                </h3>
-                            </div>
-                            <div >
-                                <div className="text-orange-500 hover:text-orange-700 py-2 px-5 bg-white shadow rounded-xl  whitespace-nowrap dark:hover:text-white dark:bg-black dark:border-white dark:hover:bg-orange-700 dark:border ">{ t('goToCourse')}</div>
-                            </div>
-
-                        </Link>)}
-                </ul>
-            </div>
+      {courses?.length === 0 && (
+        <div className="flex-1 min-h-full flex justify-center ">
+          <h2>{t("noResults")}</h2>
         </div>
-    );
+      )}
+      <div>
+        <ul className="flex flex-col gap-5">
+          {courses?.map((course) => (
+            <Link
+              key={course.code}
+              href={`/course/${course.code}`}
+              className="flex sm:flex-row gap-6 flex-col items-center justify-between border rounded-xl dark:bg-black dark:border-white dark:hover:bg-orange-700 border-gray-200 bg-orange-50/20 py-5 px-4 shadow-sm sm:gap-2 hover:bg-gray-50 hover:shadow transition-all duration-300"
+            >
+              <div>
+                <h2>{course.name}</h2>
+                <h3 className="font-medium">{course.code}</h3>
+              </div>
+              <div>
+                <div className="text-orange-500 hover:text-orange-700 py-2 px-5 bg-white shadow rounded-xl  whitespace-nowrap dark:hover:text-white dark:bg-black dark:border-white dark:hover:bg-orange-700 dark:border ">
+                  {t("goToCourse")}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const q = ctx.query.q as string;
+  const searchTerm = q.split(" "); /*.map(w => `'${w}'`).join(' | ');*/
+  let sbq = createServerSupabaseClient<Database>(ctx)
+    .from("courses")
+    .select("*");
+
+  searchTerm.forEach((w) => {
+    sbq = sbq.or(`name.ilike.%${w}%,code.ilike.%${w}%`);
+  });
+
+  const { data: courses } = await sbq;
+
+  return {
+    props: {
+      courses: courses ?? [],
+      ...(await serverSideTranslations(ctx.locale as string, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
-export async function getStaticProps({ locale }: any) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, ['common'])),
-            // Will be passed to the page component as props
-        },
-    };
-}
+
 export default Search;
