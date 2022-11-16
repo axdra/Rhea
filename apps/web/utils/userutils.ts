2d2ae0bf6609e2fe696a7b45b36708ef15ca39a1
axeldraws <axel.draws@outlook.com>
2022-11-16 22:39:20 +0100

    now encrypts session

--- apps/web/utils/userutils.ts
+++ apps/web/utils/userutils.ts
@@ -1,13 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
+import { encryptKronoxSession } from "./src/crypto";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.SUPABASE_SECRET_KEY || "";
 
 export const updateKronoxSession = async (session: string, user_token:string) => {
     const supabase = createClient(supabaseUrl, supabaseKey)
+
     supabase.auth.getUser(user_token).then(data => {
         if (data.data.user?.aud === 'authenticated') {
-            supabase.from('kronox_users').update({kronox_session: session, last_poll: new Date()}).eq('user',data.data.user.id ).then(data => {});
-            supabase.from('kronox_users').insert({kronox_session: session, user: data.data.user.id}).then(data => {
+            const encryptedSession = encryptKronoxSession(session);
+            supabase.from('kronox_users').update({kronox_session: encryptedSession, last_poll: new Date()}).eq('user',data.data.user.id ).then(data => {});
+            supabase.from('kronox_users').insert({kronox_session: encryptedSession, user: data.data.user.id}).then(data => {
             })
         
         }     
