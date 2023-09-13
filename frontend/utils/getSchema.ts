f6ef48821ffece2d5f96f088ca2f40a2a7bc9efd
Axel Draws <axel.draws@outlook.com>
2023-09-13 09:21:53 +0200

    Compound events to one if its the same but adds rooms to one event

--- frontend/utils/getSchema.ts
+++ frontend/utils/getSchema.ts
@@ -2,20 +2,20 @@ import { createClient } from "@supabase/supabase-js";
 import dayjs from "dayjs";
 import { IEvent } from "../components/schema";
 //@ts-ignore IDK ABOUT THIS, THIS IS SO WEIRD, IT WORKS BUT I DONT KNOW WHY IT WORKS DONT TOUCH IT BAD PRACTICE...
-import ical, { CalendarResponse }  from 'node-ical';
+import ical, { CalendarResponse } from 'node-ical';
 
 interface ISchema {
     name: string;
     events: IEvent[];
-    
+
 }
 
 const URLBase = "https://webbschema.mdu.se/setup/jsp/SchemaICAL.ics?startDatum=idag&intervallTyp=a&intervallAntal=1&sprak=SV&sokMedAND=true&forklaringar=true&resurser=k.";
-const getEventFromIcalEvent = (event: any,parentCode:string): any => {
+const getEventFromIcalEvent = (event: any, parentCode: string): any => {
     const parsedEvent = {
         created_at: event.created,
         parent_calendar: parentCode,
-        name:  event.summary.split('Moment: ')[1].split("Aktivitetstyp")[0].trim().replace(/&amp;/g, '&') || '',
+        name: event.summary.split('Moment: ')[1].split("Aktivitetstyp")[0].trim().replace(/&amp;/g, '&') || '',
         room: event.location,
         location: event.location,
         last_update: event.lastmodified,
@@ -25,37 +25,38 @@ const getEventFromIcalEvent = (event: any,parentCode:string): any => {
         teacher: event.summary.split('Sign:')[1]?.split("Moment: ")[0].trim(),
         group: "",
         id: event.uid
-        
+
     }
     return parsedEvent;
 }
 const updateSchemaCache = async (code: string): Promise<ISchema | undefined> => {
     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
     const supabaseAnonKey = process.env.SUPABASE_SECRET_KEY;
-    
+
     if (!supabaseUrl || !supabaseAnonKey) {
         throw new Error('Missing supabase environment variables')
     }
     const supabase = createClient(supabaseUrl, supabaseAnonKey)
     const calendar = await ical.fromURL(URLBase + code)
-    const events = Object.values(calendar).filter((event:any) => event.type === 'VEVENT').map((event) => getEventFromIcalEvent(event,code))
-    if(events.length == 0){
+    const events = cleanDuplicateEvents(Object.values(calendar).filter((event: any) => event.type === 'VEVENT').map((event) => getEventFromIcalEvent(event, code)));
+    if (events.length == 0) {
         throw new Error("No new events in caching..");
     }
     await supabase.from('events').delete().match({ parent_calendar: code });
-    await supabase.from('events').insert(events.map((calevent:any) =>{
+    await supabase.from('events').insert(events.map((calevent: any) => {
         return {
-        ...calevent,
-        id:code+calevent.id 
-    }}));
+            ...calevent,
+            id: code + calevent.id
+        }
+    }));
     await supabase.from('calendars').update({ last_cache: dayjs().format() }).match({ code: code })
-    return {events:events,name:code}
-        
- 
+    return { events: events, name: code }
+
+
 }
 
 const getSchema = async (code: string): Promise<ISchema | undefined> => {
-       
+
     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
     const supabaseAnonKey = process.env.SUPABASE_SECRET_KEY
 
@@ -63,33 +64,60 @@ const getSchema = async (code: string): Promise<ISchema | undefined> => {
         throw new Error('Missing supabase environment variables')
     }
     const supabase = createClient(supabaseUrl, supabaseAnonKey)
-    
+
     const calendar = await supabase.from('calendars').select("*, events(*)").limit(1).ilike('code', code).single()
     if (calendar.data) {
-    if (calendar?.data?.last_cache === undefined || calendar?.data?.last_cache === null || Math.abs(dayjs(calendar.data.last_cache).diff(dayjs(), 'hour')) >= 1) {
-        console.log("Updating cache for calendar: " + code)
-             return updateSchemaCache(code).then((data) => {
+        if (calendar?.data?.last_cache === undefined || calendar?.data?.last_cache === null || Math.abs(dayjs(calendar.data.last_cache).diff(dayjs(), 'hour')) >= 1) {
+            console.log("Updating cache for calendar: " + code)
+            return updateSchemaCache(code).then((data) => {
                 return data
             }).catch((err) => {
                 throw err
             }
             )
-    } else {
-        console.log("Using cached data for calendar: " + code)
-        const events = await supabase.from('events').select('*').ilike('parent_calendar', code)
-        if(events.data){
-            return { events: events.data, name: code }
         } else {
-            return undefined
+            console.log("Using cached data for calendar: " + code)
+            const events = await supabase.from('events').select('*').ilike('parent_calendar', code)
+            if (events.data) {
+                return { events: events.data, name: code }
+            } else {
+                return undefined
+            }
         }
-    }
     } else {
         return undefined
     }
 
-    
+
 
 }
 
 
 export default getSchema;
+function cleanDuplicateEvents(events: any[]): any[] {
+    let retEvents: any[] = [];
+    events.forEach(element => {
+        let filteredEvents = retEvents.filter(ev => {
+            if (element.start_time == ev.start_time &&
+                element.name == ev.name &&
+                element.end_time == ev.end_time &&
+                element.teacher == ev.teacher)
+                return true;
+            return false;
+        });
+        if (filteredEvents.length > 0) {
+            for (let index = 0; index < retEvents.length; index++) {
+                if(retEvents[index].id == filteredEvents[0].id){
+                    retEvents[index].room += `, ${element.room}`
+                    retEvents[index].location += `, ${element.location}`
+                }
+                
+            }
+        }else{
+            retEvents.push(element);
+        }
+    });
+
+    return retEvents;
+}
+
